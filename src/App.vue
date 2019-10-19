f7e28e44d663bc85b4c37e32f178351368be4cf6
Vaztonyo <tony.dewet7@gmail.com>
2019-10-19 11:57:48 +0200

    Added To do Item and styling to the list + a markComplete function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="img1" alt="Todo logo" src="./assets/List-PNG-Transparent-Picture.png">
+    <TodoList v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+      box-sizing: border-box;
+      margin: 5;
+      padding: 5;
+    }
+
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+      line-height: 1.4;
+    }
+    #img1 {
+      width: 20%;
+      height: 20%;
+    }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <div v-bind:key="todo.id" v-for="todo in todos">
+    <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+    </div>
+  </div>
+</template>
+
+<script type = "text/javascript" >
+import TodoItem from './TodoItem.vue';
+
+export default {
+  name: "TodoList",
+  components: {
+    TodoItem
+  },
+  props: ["todos"]
+};
+</script>
+<style scoped>
+</style>
